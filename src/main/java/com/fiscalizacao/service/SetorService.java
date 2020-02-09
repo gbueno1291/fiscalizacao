a1732ec7630b6bd0f724003691f130b5909014fa
unknown <giovani_bueno@live.com>
2020-02-09 17:57:10 -0300

    arquvivos modificados

--- src/main/java/com/fiscalizacao/service/SetorService.java
+++ src/main/java/com/fiscalizacao/service/SetorService.java
@@ -23,4 +23,31 @@ public class SetorService {
 		return set;
 	}
 	
+	public Setor salvarSetor(Setor setor) {
+		
+		  Setor novoSetor = new Setor();
+		try {
+			
+		    novoSetor = setorRepository.save(setor);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return  novoSetor;
+	}
+	public void  deletarSetor(Integer id) {
+        setorRepository.deleteById(id);
+       
+//	     if(id != null ) {
+//		    setor = setorRepository.findById(id).orElse(null);
+//		    if(setor !=null) {
+//		    	try {
+//				    setorRepository.delete(setor);
+//				} catch (Exception e) {
+//					e.printStackTrace();
+//				}
+//				return  setor;
+//		    }
+//		 }
+//         return null;
+	}
 }
