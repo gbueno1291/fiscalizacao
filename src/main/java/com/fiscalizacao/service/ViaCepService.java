af9267c06302b87a801241dc6a58f121e29756de
Giovani <giovani_bueno@live.com>
2020-02-12 20:32:26 -0300

    Realizado mudanças para buscar cep  api viacep

--- src/main/java/com/fiscalizacao/service/ViaCepService.java
+++ src/main/java/com/fiscalizacao/service/ViaCepService.java
@@ -1,35 +1,28 @@
 package com.fiscalizacao.service;
 
+import java.text.Normalizer;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import com.fiscalizacao.configs.URLProperties;
-import com.fiscalizacao.configs.URLProperties.ApiExterna;
 import com.fiscalizacao.dto.EnderecoDTO;
 
 @Service
 public class ViaCepService {
-	
-	@Autowired
-	private ApiExterna apiExterna;
-	
-	public List<EnderecoDTO> getEndereço(String cep) {
+		
+	public List<EnderecoDTO> getCepByEndereco(String estado, String cidade,String endereco) {
 		RestTemplate restTemplate = new RestTemplate();
 		
-		String uri = apiExterna.getExterior().get("viacep");
+		String uri = montaURL(estado, cidade, endereco);
 		
-
-		Map<String, String> params = new HashMap<String, String>();
-		params.put("cep", cep);
 		List<EnderecoDTO> enderecosDTO = new ArrayList<>();
 		try {
 			ResponseEntity<List<EnderecoDTO>> listEnderecoRest = restTemplate.exchange(uri, HttpMethod.GET, null,
@@ -44,5 +37,28 @@ public class ViaCepService {
 		return enderecosDTO;
 
 	}
-
+	public ResponseEntity<EnderecoDTO> getEnderecoByCep(String cep) {
+		 RestTemplate restTemplate = new RestTemplate();
+		    
+		    String uri = "http://viacep.com.br/ws/{cep}/json/";
+		    
+		    Map<String, String> params = new HashMap<String, String>();
+		    params.put("cep", cep);
+		    EnderecoDTO enderecoDTO = restTemplate.getForObject(uri, EnderecoDTO.class, params);
+		    return new ResponseEntity<EnderecoDTO>(enderecoDTO, HttpStatus.OK);
+	}
+	
+	public  String montaURL(String estado,String cidade,String endereco) {
+		String cidadeNormalize  = "";
+		String enderecoNormalize = "";
+		if(!cidade.isEmpty()) {
+			cidadeNormalize = Normalizer.normalize(cidade, Normalizer.Form.NFD).replaceAll("[^\\p{ASCII}]", "");
+		}
+		if(!endereco.isEmpty()) {
+			enderecoNormalize = Normalizer.normalize(endereco, Normalizer.Form.NFD).replaceAll("[^\\p{ASCII}]", "");
+		}
+		
+		String url = "https://viacep.com.br/ws/"+estado+"/"+cidadeNormalize+"/"+enderecoNormalize+"/json/";
+	    return url;
+	}
 }
