f010dfd630add3463219ac79afdc82509283bec9
giovani <giovani_bueno@live.com>
2020-02-28 19:05:51 -0300

    Auto stash before merge of "develop" and "origin/feature/remodelagemDB"

--- src/main/java/com/fiscalizacao/models/Contribuinte.java
+++ src/main/java/com/fiscalizacao/models/Contribuinte.java
@@ -1,84 +1,25 @@
 package com.fiscalizacao.models;
 
-import java.util.List;
-
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
 
 import lombok.Getter;
 import lombok.Setter;
 
-@Entity
 @Getter
 @Setter
+@Entity
 @Table(name="contribuinte")
 public class Contribuinte {
-
-	public static final long SerialVersionUID = 1L;
 	
+	private static final long serialVersionUID = 1L;
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	
-	@Column(name="cadastro")
-	@NotNull
-    private int cadastro; 
 	
-	@Column(name="nome")
-	@NotNull
-    private String nome;
-	
-	@Column(name="cpf")
-	@NotNull
-    private String cpf; 
-	
-	@Column(name="celular")
-	@NotNull
-    private String celular;
-	
-	@Column(name="telefone")
-	@NotNull
-    private String telefone; 
-	
-	@Column(name="email")
-	@NotNull
-    private String email; 
-	
-	@Column(name="cep")
-	@NotNull
-    private String cep; 
-	
-	@Column(name="logradouro")
-	@NotNull
-    private String logradouro; 
-	
-	@Column(name="numero")
-	@NotNull
-    private String numero;
-	
-	@Column(name="complemento")
-	@NotNull
-    private String complemento; 
-	
-	@Column(name="bairro")
-	@NotNull
-    private String bairro;
-	
-	@Column(name="cidade")
-	@NotNull
-    private String cidade; 
-	
-	@Column(name="uf")
-	@NotNull
-    private String uf; 
-	
-	@OneToMany(mappedBy="contribuinteid", fetch = FetchType.LAZY)
-	public List<Imovel> imovel;
-} 
+
+}
